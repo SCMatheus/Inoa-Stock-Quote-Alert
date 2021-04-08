6cd725efaf4b892db69bddda2327610389090db3
SCMatheus <matheusouzacardoso2016@gmail.com>
2021-04-08 13:25:37 -0300

    Criação do projeto de testes

---
+++ Teste.Inoa.StockQuoteAlert/Teste.Inoa.StockQuoteAlert.TestesUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Teste.Inoa.StockQuoteAlert.TestesUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Teste.Inoa.StockQuoteAlert.TestesUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bb60400-630b-4f6e-91c9-235df7c57802")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Teste.Inoa.StockQuoteAlert/Teste.Inoa.StockQuoteAlert.TestesUnitarios/StockQuoteAlertTeste.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Teste.Inoa.StockQuoteAlert.TestesUnitarios
+{
+    [TestClass]
+    public class StockQuoteAlertTeste
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
