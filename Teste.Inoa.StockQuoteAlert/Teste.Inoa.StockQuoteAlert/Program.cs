5fbab56f5d856b8f790655ccf0c1ece08d8ab6b1
SCMatheus <matheusouzacardoso2016@gmail.com>
2021-04-07 23:08:25 -0300

    Criação do projeto console

---
+++ Teste.Inoa.StockQuoteAlert/Teste.Inoa.StockQuoteAlert/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.Inoa.StockQuoteAlert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
